d2b8ba1ad5b268edef62c0da343bddea355863e7
Pigmassacre <gulligaolle@gmail.com>
2013-10-11 15:16:03 +0200

    FINALLY GOT A WORKING EXAMPLE. HOLY SHIT. IT WORKS. I AM HAPPY.

--- WebTicTacToe/src/main/java/com/github/webtictactoe/webtictactoe/LobbyResource.java
+++ WebTicTacToe/src/main/java/com/github/webtictactoe/webtictactoe/LobbyResource.java
@@ -1,46 +1,60 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.github.webtictactoe.webtictactoe;
 
-import javax.ws.rs.FormParam;
+import org.atmosphere.annotation.Broadcast;
+import org.atmosphere.annotation.Suspend;
+import org.atmosphere.config.service.AtmosphereService;
+import org.atmosphere.cpr.AtmosphereResourceEvent;
+import org.atmosphere.cpr.AtmosphereResourceEventListenerAdapter;
+import org.atmosphere.jersey.JerseyBroadcaster;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import org.atmosphere.annotation.Broadcast;
-import org.atmosphere.cpr.Broadcaster;
-import org.atmosphere.jersey.Broadcastable;
-import org.atmosphere.jersey.SuspendResponse;
 
 /**
- * Testing...
- * 
- * @author pigmassacre
+ * Simple chat resource demonstrating the power of Atmosphere. This resource supports transport like WebSocket, Streaming, JSONP and Long-Polling.
+ *
+ * @author Jeanfrancois Arcand
  */
-
-@Path("/{topic}")
-@Produces("application/json")
+@Path("/")
+@AtmosphereService (broadcaster = JerseyBroadcaster.class)
 public class LobbyResource {
-    
-    private @PathParam("topic") Broadcaster topic;
-    
+
+    /**
+     * Suspend the response without writing anything back to the client.
+     *
+     * @return a white space
+     */
+    @Suspend(contentType = "application/json", listeners = {OnDisconnect.class})
     @GET
-    public SuspendResponse<String> subscribe() {
-        return new SuspendResponse
-                .SuspendResponseBuilder<String>()
-                .broadcaster(topic)
-                .outputComments(true)
-                .build();
+    public String suspend() {
+        System.out.println("suspend() called");
+        return "";
     }
-    
+
+    /**
+     * Broadcast the received message object to all suspended response. Do not write back the message to the calling connection.
+     *
+     * @param message a {@link Message}
+     * @return a {@link Response}
+     */
+    @Broadcast(writeEntity = false)
     @POST
-    @Broadcast
-    @Produces("text/html;charset=ISO-8859-1")
-    public Broadcastable publish(@FormParam("message") String message) {
-        return new Broadcastable(message, "", topic);
+    @Produces("application/json")
+    public Response broadcast(Message message) {
+        System.out.println("broadcast() called: message received: " + message.author + " says " + message.message);
+        return new Response(message.author, message.message);
     }
-    
+
+    public static final class OnDisconnect extends AtmosphereResourceEventListenerAdapter {
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void onDisconnect(AtmosphereResourceEvent event) {
+            System.out.println(event);
+        }
+    }
+
 }
