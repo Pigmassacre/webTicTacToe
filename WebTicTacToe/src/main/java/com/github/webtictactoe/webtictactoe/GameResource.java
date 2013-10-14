34d0dbb6e871a86b2c600517981f4a21c172e557
Pigmassacre <gulligaolle@gmail.com>
2013-10-14 17:24:01 +0200

    Working on findgame.

--- WebTicTacToe/src/main/java/com/github/webtictactoe/webtictactoe/GameResource.java
+++ WebTicTacToe/src/main/java/com/github/webtictactoe/webtictactoe/GameResource.java
@@ -1,7 +1,9 @@
 package com.github.webtictactoe.webtictactoe;
 
+import com.github.webtictactoe.tictactoe.core.Game;
 import com.github.webtictactoe.tictactoe.core.GameSession;
 import com.github.webtictactoe.tictactoe.core.ILobby;
+import com.github.webtictactoe.tictactoe.core.Player;
 import java.util.HashMap;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.CookieParam;
@@ -15,20 +17,20 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import org.atmosphere.cpr.Broadcaster;
+import javax.ws.rs.core.Response;
 
-@Path("/game/{id}")
+@Path("/")
 @AtmosphereService(broadcaster = JerseyBroadcaster.class)
 public class GameResource {
     
     private static ILobby lobby = Lobby.INSTANCE.getLobby();
     private static HashMap<String, GameSession> gameSessionMap = new HashMap<String, GameSession>();
-    private @PathParam(value = "id") String id;
     private @CookieParam(value = "name") String name;
     
     @GET
     @Suspend(contentType = "application/json")
-    public String suspend() {
+    @Path("/game/{id}")
+    public String suspend(@PathParam(value = "id") String id) {
         if (gameSessionMap.containsKey(id)) {
             
         }
@@ -37,11 +39,28 @@ public class GameResource {
     }
     
     @POST
+    @Produces("application/json")
+    @Path("/findgame/{size}")
+    public Response findGame(@PathParam(value = "size") Integer size) {
+        Player givenPlayer;
+        
+        // We get the first player matching the name (there should only ever be one anyway).
+        for (Player player : lobby.getPlayerRegistry().getByName(name)) {
+            givenPlayer = player;
+            Game game = lobby.findGame(givenPlayer, size);
+            return Response.ok().build();
+        }
+        
+        // Player matching that name not found, something terribly wrong has occured!
+        return Response.status(400).build();
+    }
+    
+    @POST
     @Broadcast(writeEntity = false)
     @Consumes("application/json")
     @Produces("application/json")
-    @Path("/move")
-    public GameResponse broadcastGamestate(GameMessage gameMessage) {
+    @Path("/game/{id}/move")
+    public GameResponse broadcastGamestate(@PathParam(value = "id") String id, GameMessage gameMessage) {
         return new GameResponse();
     }
     
