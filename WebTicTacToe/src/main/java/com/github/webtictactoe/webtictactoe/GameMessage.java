22e51344286993d07aabb224f396f7f901b902c6
Pigmassacre <gulligaolle@gmail.com>
2013-10-14 18:16:24 +0200

    GameMessage should only send x and y pos.

--- WebTicTacToe/src/main/java/com/github/webtictactoe/webtictactoe/GameMessage.java
+++ WebTicTacToe/src/main/java/com/github/webtictactoe/webtictactoe/GameMessage.java
@@ -5,12 +5,10 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 public class GameMessage {
 
-    public String name;
     public String xPos;
     public String yPos;
 
-    public GameMessage(String name, String xPos, String yPos) {
-        this.name = name;
+    public GameMessage(String xPos, String yPos) {
         this.xPos = xPos;
         this.yPos = yPos;
     }
